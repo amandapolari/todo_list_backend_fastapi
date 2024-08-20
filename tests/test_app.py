4830e9e8f83d1b8b2cf9ec76beb7f5d2e05aebd6
amandapolari <amandaoliveirapolari@gmail.com>
2024-08-20 15:24:52 -0400

    feat: crud implementation

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,13 +1,7 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from todo_list_backend_fastapi.app import app
-
-
-def test_read_root_should_return_ok_and_hello_world():
-    client = TestClient(app)
-
+def test_read_root_should_return_ok_and_hello_world(client):
     response = client.get('/')
 
     assert response.status_code == HTTPStatus.OK
@@ -15,9 +9,7 @@ def test_read_root_should_return_ok_and_hello_world():
     assert response.json() == {'message': 'Olá Mundo!'}
 
 
-def test_say_hello_should_return_ok_and_hello():
-    client = TestClient(app)
-
+def test_say_hello_should_return_ok_and_hello(client):
     response = client.get('/hello')
 
     assert response.status_code == HTTPStatus.OK
@@ -34,3 +26,62 @@ def test_say_hello_should_return_ok_and_hello():
     </html>"""
 
     assert response.text.strip() == expected_html.strip()
+
+
+def test_create_user(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'amanda',
+            'email': 'amanda@gmail.com',
+            'password': 'amanda123',
+        },
+    )
+
+    # Verificando se voltou o status code correto com os dados acima
+    assert response.status_code == HTTPStatus.CREATED
+
+    # Verificando se o que volta para o usuário é o UserPublic
+    assert response.json() == {
+        'username': 'amanda',
+        'email': 'amanda@gmail.com',
+        'id': 1,
+    }
+
+
+def test_read_users(client):
+    response = client.get('/users/')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [
+            {
+                'username': 'amanda',
+                'email': 'amanda@gmail.com',
+                'id': 1,
+            }
+        ]
+    }
+
+
+def test_update_user(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'username': 'bob',
+            'email': 'bob@example.com',
+            'password': 'mynewpassword',
+        },
+    )
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': 'bob',
+        'email': 'bob@example.com',
+        'id': 1,
+    }
+
+
+def test_delete_user(client):
+    response = client.delete('/users/1')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'User deleted'}
--- todo_list_backend_fastapi/app.py
+++ todo_list_backend_fastapi/app.py
@@ -1,16 +1,21 @@
 from http import HTTPStatus
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.responses import HTMLResponse
 
 from todo_list_backend_fastapi.schemas import (
     Message,
     TextoHtml,
+    UserDB,
+    UserList,
     UserPublic,
     UserSchema,
 )
 
 app = FastAPI()
+
+
+database = []
 
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
@@ -38,6 +43,52 @@ def say_hello():
     return HTMLResponse(content=text)
 
 
+@app.get('/users/', response_model=UserList)
+def read_users():
+    return {'users': database}
+
+
 @app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
 def create_user(user: UserSchema):
-    return user
+    # breakpoint()
+
+    user_with_id = UserDB(
+        # criando o id:
+        id=len(database) + 1,
+        # **user -> pegando todos os campos de user,
+        # chave e valor, desmembrando:
+        # .model_dump() -> Convertendo o objeto do pydantic em um dicionário:
+        **user.model_dump(),
+    )
+
+    # Adicionando os usuários ao db fake:
+    database.append(user_with_id)
+
+    return user_with_id
+
+
+@app.put('/users/{user_id}', response_model=UserPublic)
+def update_user(user_id: int, user: UserSchema):
+    # se o id for mair que o tamanho da lista ou um número negativo...
+    if user_id > len(database) or user_id < 1:
+        # raise -> subir o erro do HTTPException
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+
+    user_with_id = UserDB(**user.model_dump(), id=user_id)
+    database[user_id - 1] = user_with_id
+
+    return user_with_id
+
+
+@app.delete('/users/{user_id}', response_model=Message)
+def delete_user(user_id: int):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+
+    del database[user_id - 1]
+
+    return {'message': 'User deleted'}
